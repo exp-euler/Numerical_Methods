4abbc6378f7f11b614dd07b57bab9b8b49eca9d5
exp-euler <26654173+exp-euler@users.noreply.github.com>
2023-07-16 19:16:03 +0200

    Implement calculations of matrix Phi functions with scaling&squaring and Taylor expansion.

--- Tests/matFunTest.cpp
+++ Tests/matFunTest.cpp
@@ -1,5 +1,6 @@
 #include "Matrix.hpp"
 #include "Vector.hpp"
+#include "MatrixFunctions.hpp"
 
 int main(){
     Matrix<double> A(3,3);
@@ -8,15 +9,23 @@ int main(){
 
     A(0,0)=1; A(0,1)=2; A(0,2)=3;
     A(1,0)=1; A(1,1)=2; A(1,2)=3;
-    A(2,0)=1; A(2,1)=2; A(2,2)=3;
+    A(2,0)=1; A(2,1)=2; A(2,2)=-30;
 
     B(0,0)=1; B(0,1)=0; B(0,2)=0;
     B(1,0)=0; B(1,1)=1; B(1,2)=0;
-    B(2,0)=0; B(2,1)=0; B(2,2)=1;
+    B(2,0)=7; B(2,1)=0; B(2,2)=1;
 
     C = A*B;
+    C = A+B;
 
-    std::cout << C << "\n";
+    Matrix<double> phi1(3,3);
+    Matrix<double> phi2(3,3);
+    std::vector<Matrix<double>> phi_f = {phi1, phi2};
+    LinearAlgebra::phi_functions(phi_f, 1, A);
+
+    std::cout << A << "\n";
+    std::cout << "##########################" << "\n";
+    std::cout << phi_f[1] << "\n";
 
     return 0;
 }
---
+++ src/MatrixFunctions.hpp
@@ -0,0 +1,48 @@
+#include "Matrix.hpp"
+#include <vector>
+#include <cassert>
+#include <bits/stdc++.h>
+
+typedef Matrix<double> matrix;
+namespace LinearAlgebra {
+    void Taylor(matrix &T, matrix &L, int k) {
+        std::vector<double> coeff = {1.0, 1.0, 0.5, 1.0/6.0, 1.0/24.0, 1.0/120.0, 1.0/720.0, 1.0/5040.0, 1.0/40320.0, 1.0/362880.0, 1.0/3628800.0};
+        matrix temp(matrix::Diagonal(L.NumRows(), L.NumCols(), 1.0));
+        T = temp*coeff[0];
+        for(int i=1; i<=k; i++) {
+            temp = temp*L;
+            T = T + temp*coeff[i];
+        }
+    }
+
+    int scale(matrix &A) {
+    double normA = A.inf_norm();
+    int j = normA > 0 ? 1+round(log(normA/4)/log(2)) : 0;
+    return j >= 0 ? j : 0;
+    }
+
+    void unscale(std::vector<matrix> &phi, matrix L, int j, int l) {
+    for(int m=0; m <= l; ++m)
+        Taylor(phi[m],L,4);
+    
+    // Unscaling relations taken form:
+    // http://www.math.ntnu.no/preprint/numerics/2005/N4-2005.pdf
+    for(int k=1; k <= j; ++k) {
+        if(l >= 3)
+            phi[3] = (phi[1]*phi[2]+phi[3]*phi[1]+phi[2])*0.125;
+        if(l >= 2)
+            phi[2] = (phi[1]*phi[1]+phi[2]*2)*0.25;
+        if(l >= 1)
+            phi[1] = (phi[0]*phi[1]+phi[1])*0.5;
+        phi[0] = phi[0]*phi[0];
+        }
+
+    }
+
+    void phi_functions(std::vector<matrix> &phi, int l, matrix A) {
+    int j = scale(A);
+    j = 40; // Problem in accuracy is the scaling... Must be high enough?
+    unscale(phi, A*(1.0/double(pow(2,j))), j, l);
+    }
+
+}
